ec9eb12833e9be53067edee04883233735c0e505
failedman <2711339704@qq.com>
2026-05-27 10:59:42 +0800

    20260527

---
+++ src/main/java/top/zxylearn/ele3server/Ele3serverApplication.java
@@ -0,0 +1,13 @@
+package top.zxylearn.ele3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ele3serverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ele3serverApplication.class, args);
+	}
+
+}
